e405890e5f71040fc5616083dd58fdaeb6e5d739
igorbalden <iseedek@gmail.com>
2021-02-15 15:41:15 +0200

    Init Docker Php8

---
+++ config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Liip\FunctionalTestBundle\LiipFunctionalTestBundle::class => ['dev' => true, 'test' => true],
+    Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
+];
